dbe1c315cb6ff8fdc4fc374c383e4907cdec4679
baejungmo <bjm1244>
2022-02-26 11:43:57 +0900

    curl test add

--- curl_test.sh
+++ curl_test.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-
-RESULT=$(curl http://127.0.0.1:5000/api/total/abcd)
+ipv4_address=$(curl ipv4.icanhazip.com)
+RESULT=$(curl http://$ipv4_address:5000/api/total/abcd)
 if [ $RESULT = "{\"client_id\":\"abcd\",\"total\":1}" ]
 then
     echo "Integration Test Success"
