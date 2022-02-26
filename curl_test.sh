9887a24b2e0dd2d42d75513fbdbb0f3c0bcc136e
baejungmo <bjm1244>
2022-02-26 11:49:49 +0900

    testing add

--- curl_test.sh
+++ curl_test.sh
@@ -2,7 +2,9 @@
 #
 #
 #
-ipv4_address=$(curl ipv4.icanhazip.com)
+
+python3 app.py &
+ipv4_address=$(ip route | awk '{print $NF;exit}')
 RESULT=$(curl http://$ipv4_address:5000/api/total/abcd)
 if [ $RESULT = "{\"client_id\":\"abcd\",\"total\":1}" ]
 then
@@ -12,3 +14,4 @@ else
     echo "Integration Test Failed"
     exit 100
 fi
+killall -9 python3 python
